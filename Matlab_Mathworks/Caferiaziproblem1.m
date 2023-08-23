a2a20143641803002edd526a67ef9d057e736cb2
Arash The Data Engineeer! <arashthedataengineer.mail@gmail.com>
2023-08-24 02:31:23 +0330

    sign by Me

--- Matlab_Mathworks/Caferiaziproblem1.m
+++ Matlab_Mathworks/Caferiaziproblem1.m
@@ -1,35 +1,64 @@
 X = zeros(11,11);
-% Making our 11 by 11 matrix all zeros
+// Making our 11 by 11 matrix all zeros
 
-c = 0; % Our counter gonna be c
+c = 0; // Our counter gonna be c
 r = randi(11);
 b = randi(11);
-% Having the random numbers for making our one possible prize location!
+// Having the random numbers for making our one possible prize location!
 
 X(r,b) = 1;
-% Now one of the 121 possible houses is 1 and others are all zero...
+// Now one of the 121 possible houses is 1 and others are all zero...
 
-% Then predict the location by searching X matrix!
 
-for i = 1:11
-    
-    for j = 1:11
-        
-        if X(i,j) == 1
-            disp(i)
-            disp(j)
-            break;
-        else
-        end
+// Defining rect_check class that checks a rectangle if it has number 1 in it or not
 
-    end 
-
-    c = c + 1;
-    
-    if X(i,j) == 1
-        break;
-    end
-
+classdef RectCheck
+   properties   // Take our rectangle info 
+      r1; // row1
+      c1; // column1
+      r2; // ...
+      c2; // ...
+   end
+   methods
+        for i = r1:r2
+            for j = c1:c2
+            
+                if X(i,j) == 1
+                    disp('Yes!');
+                    c = c + 1;
+                end
+            
+            end
+        end 
+         
+   end
 end
 
-disp(c);
+
+//Then predict the location by searching X matrix!
+
+
+// These down here are commented for next use!!::
+
+// for i = 1:11
+    
+//     for j = 1:11
+        
+//         if X(i,j) == 1
+//             disp(i)
+//             disp(j)
+//             break;
+//         else
+//         end
+
+//     end 
+
+//     c = c + 1;
+    
+//     if X(i,j) == 1
+//         break;
+//     end
+
+// end
+
+// disp(c);
